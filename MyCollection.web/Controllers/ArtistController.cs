f9bbad8284d702ef5ae23d8c55b216322725718b
Thomas Praet <t.praet@icloud.com>
2025-04-24 19:58:54 +0200

    CRUD operations all done

--- MyCollection.web/Controllers/ArtistController.cs
+++ MyCollection.web/Controllers/ArtistController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 using MyCollection.web.Data;
 using MyCollection.web.Models;
 
@@ -9,7 +10,7 @@ namespace MyCollection.web.Controllers
         private readonly ApplicationDbContext _db;
         public ArtistController(ApplicationDbContext db)
         {
-                _db = db;
+            _db = db;
         }
         public IActionResult Index()
         {
@@ -20,6 +21,63 @@ namespace MyCollection.web.Controllers
         public IActionResult Create()
         {
             return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Artist obj)
+        {
+            _db.Artists.Add(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index", "Artist");
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Artist? artistFromDb = _db.Artists.FirstOrDefault(u => u.Id == id);
+            if (artistFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(artistFromDb);
+        }
+        [HttpPost]
+        public IActionResult Edit(Artist obj)
+        {
+            _db.Artists.Update(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index", "Artist");
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Artist? artistFromDb = _db.Artists.FirstOrDefault(u => u.Id == id);
+            if (artistFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(artistFromDb);
+        }
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePOST(int? id)
+        {
+            Artist? obj = _db.Artists.Find(id);
+
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            _db.Artists.Remove(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index", "Artist");
+
         }
     }
 }
--- MyCollection.web/Models/Artist.cs
+++ MyCollection.web/Models/Artist.cs
@@ -1,12 +1,14 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace MyCollection.web.Models
 {
     public class Artist
     {
         public int Id { get; set; }
-
+        [DisplayName("First Name")]
         public string FirstName { get; set; }
+        [DisplayName("Last Name")]
         [Required]
         public string LastName { get; set; }
     }
